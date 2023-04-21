07e4f7d631bbecdf5ff8f98bef9f41049f13a45c
jose-rich <joserichard@ideas2it.com>
2023-04-21 18:15:36 +0530

    profiles added

--- auth-service/src/main/java/com/microservice/authservice/AuthServiceApplication.java
+++ auth-service/src/main/java/com/microservice/authservice/AuthServiceApplication.java
@@ -3,12 +3,14 @@ package com.microservice.authservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableEurekaClient
 @EnableJpaAuditing
+@RefreshScope
 public class AuthServiceApplication {
 
 	public static void main(String[] args) {
